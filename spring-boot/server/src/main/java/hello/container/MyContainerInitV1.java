0f9866783c0dbc4b9adc8e821041d532214b80bd
102092 <quaint3@gmail.com>
2023-08-10 21:21:19 +0900

    Add MyContainerInitV1

---
+++ spring-boot/server/src/main/java/hello/container/MyContainerInitV1.java
@@ -0,0 +1,19 @@
+package hello.container;
+
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+
+import java.util.Set;
+
+public class MyContainerInitV1 implements ServletContainerInitializer {
+
+    // WAS 가 실행될 때, 실행된다
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+        System.out.println("MyContainerInitV1.onStartUp");
+        System.out.println("MyContainerInitV1 c = " + c);
+        System.out.println("MyContainerInitV1 ctx = " + ctx);
+
+    }
+}
